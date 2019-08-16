6e340636acb109533f50d2d13dfe67e8313e7b8e
Dacredible <jikun.95.02@gmail.com>
2019-08-15 17:49:11 -0700

    Updates

---
+++ precache-manifest.f7924843b94d895fc1c4749ae8b61b76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5fb98807b8c9ba0d4f81d6d715a6b6",
+    "url": "/trello-board/index.html"
+  },
+  {
+    "revision": "5b49399a8819b891c5b2",
+    "url": "/trello-board/static/css/main.d63333fe.chunk.css"
+  },
+  {
+    "revision": "1bfda358e7e65f81c748",
+    "url": "/trello-board/static/js/2.653f029a.chunk.js"
+  },
+  {
+    "revision": "5b49399a8819b891c5b2",
+    "url": "/trello-board/static/js/main.ce8f170a.chunk.js"
+  },
+  {
+    "revision": "3d6445447c08826ee70c",
+    "url": "/trello-board/static/js/runtime~main.c0bdb7f7.js"
+  }
+]);
